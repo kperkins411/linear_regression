b95ed1c87e4a142db77f15184f8571e82a7764e0
Keith <kperkins411@gmail.com>
2019-01-30 21:33:12 -0500

    Vectorized version

--- linear_regression.py
+++ linear_regression.py
@@ -10,44 +10,9 @@ import random
 import constants
 import utils
 import numpy as np
-
 import matplotlib.pyplot as plt
-def _setplotlimits(x,y):
-    '''
-    make sure plot scale is fixed so line moves
-    :param x:
-    :param y:
-    :return:
-    '''
-    plt.xlim(min(x), max(x))
-    plt.ylim(min(y),max(y))
 
-    # Don't mess with the limits!
-    plt.autoscale(False)
-
-def plotdata(x,y, w1, w2, lr, tot_error,epoch_numb):
-    '''
-    dynamicly plots data with a bit of a pause
-    '''
-    plt.clf()
-    _setplotlimits(x,y)
-    #here is the linear regressed line
-    l = [w1*n+w2 for n in x]
-
-    plt.legend
-
-    plt.scatter(x, y)   #the random data
-    plt.plot(x,l,'-r')  #the line
-
-    #have it begin 5% in from left and 15% down from top  "final w1={:.7}".format(w1)
-    plt.text(.05*constants.NUMB_SAMPLES, .85*max(y), "Learning rate ={:.7}".format(lr)+ "\n"
-                                                     "Total error   ={:.7}".format(tot_error)+ "\n"
-                                                     "Epoch number  ={:}".format(epoch_numb))
-
-    plt.pause(0.1)
-
-
-def main(x,y,w1,w2,lr):
+def main_vectorize(x,y,w,lr):
     '''
     dynamically plot linear regression
     :param x:
@@ -63,95 +28,44 @@ def main(x,y,w1,w2,lr):
 
     cnt = 0
     for epoch_num in range(constants.NUM_EPOCHS):
-        w1, w2 = backprop(lr, w1, w2, x, y)
-        tot_error = utils.gettotalerror(x,y,w1,w2)
-        print(f"w1={w1}  w2={w2} totalerror={tot_error}")
+        w = backprop_vectorize(lr, w, x, y)
+        tot_error = np.sum((1/2)*(y-w@x)**2, axis=1)/len(x[0])
 
         if(cnt%5==0):
-            plotdata(x,y,w1,w2, lr, tot_error,epoch_num)
+            utils.plotdata(x[0,:],y,w[0,0],w[0,1], lr, tot_error[0],epoch_num)
+            # print(f"w1={w[0,0]}  w2={w[0,1]} totalerror={tot_error}")
+
+        # if(cnt%40 == 0):
+        #     # see if learning rate has changed
+        #     lr = utils.find_learning_rate_vectorize(x, y, w, backprop_vectorize, plt_lrs = False)
         cnt+=1
 
     plt.ioff()
     plt.show()
 
-def find_learning_rate(x,y,w1_init,w2_init,lr_epochs = constants.LR_EPOCHS,max_lr=constants.MAX_LR,
-                       min_lr=constants.MIN_LR, test_cycles=constants.FEW_TEST_CYCLES):
-    '''
-    creates a list of learning rates from min_lr to max_lr. Then checks each for LR_EPOCHS to calculate totalError
-    the lr with the smallest total error is the one that will converge the fastest
-    :param x: data,x
-    :param y: data,y
-    :param w1_init:
-    :param w2_init:
-    :param lr_epochs:
-    :param max_lr:
-    :param min_lr:
-    :param test_cycles:
-    :return:
-    '''
-    LR= np.linspace(min_lr, max_lr, lr_epochs).tolist() #evenly spaced list of lr's
-    totalerrors=[]
-    for lr in LR:
-        w1, w2 = w1_init, w2_init  # create weights
 
-        #train for a few epochs to see what kind of error we get
-        #if converging, then last epoch should have lowest error for this lr
-        for _ in range(test_cycles):
-            w1, w2 = backprop(lr, w1, w2, x, y)
-
-        totalerrors.append(utils.gettotalerror(x,y,w1,w2))
-
-        print(f"w1={w1}  w2={w2} lr={lr} totalerror={utils.gettotalerror(x,y,w1,w2)}")
-
-    #here is the smallest error
-    smallest_index = totalerrors.index(min(totalerrors))
-    best_lr = LR[smallest_index]
-    print(f"best learning rate={best_lr}")
-
-    #enable below to see plot
-    plt.title("Learning rate finder ")
-    plt.ylabel("error")
-    plt.xlabel("learning rate")
-    plt.ylim(min(totalerrors)-30,min(totalerrors)+30)  #lowest learning rate sets highest error (to display)
-    plt.xlim(min(LR), max(LR))
-    plt.autoscale(False)
-    plt.plot(LR, totalerrors, '-r')  # the line
-
-    plt.annotate(
-        'The minimum error',
-        xy=(best_lr, min(totalerrors)), arrowprops=dict(arrowstyle='->'), xytext=(best_lr, min(totalerrors)-5))
-    plt.show()
-
-    return best_lr
-
-
-def backprop(lr, w1, w2, x, y):
+def backprop_vectorize(lr, w, x, y):
     '''
     backprop over errorfunction to adjust w1 and w2
     :param lr: learning rate
-    :param w1: weight1
-    :param w2: weight2
-    :param x: dependent
-    :param y: independent
+    :param w: weight vector (same size as x, add 1 to end if necessary)
+    :param x: values vector dependent
+    :param y: results vector independent
     :return:
     '''
-    # calc the tot gradient
-    sum_w1 = 0
-    sum_w2 = 0
-    for i in range(constants.NUMB_SAMPLES):
-        #sum de/dw1 and de/dw2
-        sum_w1 += -(y[i] - w1 * x[i] - w2) * x[i]
-        sum_w2 += -(y[i] - w1 * x[i] - w2)
+    #the first param has all the values, get the number of columns for the number of samples
+    numb_samples=len(x[0])
+
+    # sum derivatives
+    total = np.sum(-(y - w @ x) * x, axis=1)
 
     # divide by numb_samples
-    grad_w1 = sum_w1 / constants.NUMB_SAMPLES
-    grad_w2 = sum_w2 / constants.NUMB_SAMPLES
+    grad_w = total / numb_samples
 
     # adjust w1 and w2
-    w1 = w1 - lr * grad_w1
-    w2 = w2 - lr * grad_w2
-    return w1, w2
+    w = w - np.transpose(lr * grad_w)
 
+    return w
 
 if __name__=="__main__":
     random.seed(1)  #repeatability
@@ -159,8 +73,12 @@ if __name__=="__main__":
     x, y = utils.gendata()  # create random data
     w1, w2 = constants.INITIAL_WEIGHTS  # initial weights
 
-    #first find the largest learning rate that allows model convergence
-    best_lr = find_learning_rate(x,y,w1,w2)
+    w = np.array([[w1, w2]])
+    x = np.asarray(x)
+    x = np.vstack((x, np.ones((x.shape[0]), dtype=x.dtype)))
+
+    # first find the largest learning rate that allows model convergence
+    best_lr = utils.find_learning_rate_vectorize(x, y, w, backprop_vectorize, plt_lrs=False)
 
     #run the regressor
-    main(x,y,w1,w2,lr=best_lr)
+    main_vectorize(x, y, w, lr=best_lr)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import constants
 import random
+import numpy as np
 from random import gauss
+import matplotlib.pyplot as plt
+
 
 def gendata():
     '''
@@ -22,16 +25,97 @@ def gen_weights(initial_val = 0.5,num_weights=2):
     #make this a little more random?
     return[initial_val for _ in range(num_weights)]
 
-def gettotalerror(x,y,w1,w2):
+def gettotalerror_vectorize(x,y,w):
     '''
     returns the total regressed error over the dataset x,y for the given params w1,w2
     :param x:
     :param y:
-    :param w1:
-    :param w2:
+    :param w
     :return:
     '''
-    error=0
-    for i in range(constants.NUMB_SAMPLES):
-        error += (y[i] - w1*x[i]-w2)**2
-    return error/len(x)
+    return np.sum((1 / 2) * (y - w @ x) ** 2, axis=1) / len(x[0])
+
+def _setplotlimits(x,y):
+    '''
+    make sure plot scale is fixed so line moves
+    :param x:
+    :param y:
+    :return:
+    '''
+    plt.xlim(min(x), max(x))
+    plt.ylim(min(y),max(y))
+
+    # Don't mess with the limits!
+    plt.autoscale(False)
+
+def plotdata(x,y, w1, w2, lr, tot_error,epoch_numb):
+    '''
+    dynamicly plots data with a bit of a pause
+    '''
+    plt.clf()
+    _setplotlimits(x,y)
+    #here is the linear regressed line
+    l = [w1*n+w2 for n in x]
+
+    plt.legend
+
+    plt.scatter(x, y)   #the random data
+    plt.plot(x,l,'-r')  #the line
+
+    #have it begin 5% in from left and 15% down from top  "final w1={:.7}".format(w1)
+    plt.text(.05*constants.NUMB_SAMPLES, .85*max(y), "Learning rate ={:.7}".format(lr)+ "\n"
+                                                     "Total error   ={:.7}".format(tot_error)+ "\n"
+                                                     "Epoch number  ={:}".format(epoch_numb))
+
+    plt.pause(0.01)
+
+def find_learning_rate_vectorize(x,y,w_init,backprop_func, lr_epochs = constants.LR_EPOCHS,max_lr=constants.MAX_LR,
+                       min_lr=constants.MIN_LR, test_cycles=constants.FEW_TEST_CYCLES, plt_lrs = True):
+    '''
+    creates a list of learning rates from min_lr to max_lr. Then checks each for LR_EPOCHS to calculate totalError
+    the lr with the smallest total error is the one that will converge the fastest
+    :param x: data,x
+    :param y: data,y
+    :param w
+    :param backprop_func function that calculates gradient for params
+    :param lr_epochs:
+    :param max_lr:
+    :param min_lr:
+    :param test_cycles:
+    :return:
+    '''
+    LR= np.linspace(min_lr, max_lr, lr_epochs).tolist() #evenly spaced list of lr's
+    totalerrors=[]
+    for lr in LR:
+        w=w_init  # create weights
+
+        #train for a few epochs to see what kind of error we get
+        #if converging, then last epoch should have lowest error for this lr
+        for _ in range(test_cycles):
+            w = backprop_func(lr, w, x, y)
+
+        err = gettotalerror_vectorize(x,y,w)
+        totalerrors.append(err)
+        print(f"w1={w[0,0]}  w2={w[0,1]} lr={lr} totalerror={err}")
+
+    #here is the smallest error
+    smallest_index = totalerrors.index(min(totalerrors))
+    best_lr = LR[smallest_index]
+    print(f"best learning rate={best_lr}")
+
+    #enable below to see plot
+    if(plt_lrs==True):
+        plt.title("Learning rate finder ")
+        plt.ylabel("error")
+        plt.xlabel("learning rate")
+        plt.ylim(min(totalerrors)-30,min(totalerrors)+30)  #lowest learning rate sets highest error (to display)
+        plt.xlim(min(LR), max(LR))
+        plt.autoscale(False)
+        plt.plot(LR, totalerrors, '-r')  # the line
+
+        plt.annotate(
+            'The minimum error',
+            xy=(best_lr, min(totalerrors)), arrowprops=dict(arrowstyle='->'), xytext=(best_lr, min(totalerrors)-5))
+        plt.show()
+
+    return best_lr
